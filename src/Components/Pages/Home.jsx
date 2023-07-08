b6b4e2701d72a707276bd1ae155287afc4bc3c2d
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-07-07 18:13:54 -1000

    added intro paragraph in Container element

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import { Heading } from "@chakra-ui/react";
 import Logo from "../Logo";
-import { Flex, Box, Text } from "@chakra-ui/react";
+import { Flex, Box, Text, Container } from "@chakra-ui/react";
 
 export function Home() {
     return (
         <Flex w='100%' justify='center' gap='1em' direction='column' align='center' mt='2em'>
-            <Heading fontFamily={'libre franklin'} fontWeight="600">Keeep track of your Hours</Heading>
-            <Text>Tenure makes it easy to keep track of your working hours.</Text>
+            <Heading fontFamily={'prompt'} fontWeight="600">Track and optimize your work hours with ease.</Heading>
+            <Container fontFamily={'libre franklin'} fontSize='1.2rem' lineHeight={1.5}>With Tenure, you can effortlessly monitor and record your work hours, making it easier than ever to stay organized and efficient. Whether you're a freelancer, a remote worker or someone looking to track your productivity, Tenure offers a user-friendly interface and great features to help you keep track of your valuable time. Stay on top of your work schedule, track projects, and make the most out of your work hours with Tenure.</Container>
+
         </Flex>
     )
 }
